777ef5bd795c1850c9ac82bccf837c50a0ddec4a
Devin Ho <djxho36@gmail.com>
2022-04-22 00:07:34 -0400

    commit tree d50054d49e0917f6b6855f4d063b3cd2e2fb91f7

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+function submitData(firstName, email) {
+  const configurableObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      "name": firstName,
+      "email": email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurableObject)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => {
+     alert("Unathorized Access")
+     document.body.append(response.message);
+    });
+  }
+
+function addNewContact(newID){
+  document.getElementsByTagName("body")[0].innerHTML = newID;
+  //document.body.append(message)
+}
+
+submitData("Name", "name@name.com");
+
+
+ /* fetch("http://localhost:3000/users", configurableObject)
+    .then(response => {
+      return response.json();
+    })
+    .then(body => {
+      return addNewContact(body.id);
+    })
+    .catch(error => {
+      alert("Unathorized Access");
+      return error.message;
+    })
+  }
+  */
